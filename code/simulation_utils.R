a93e329527760bb16ec55992b8a73c380728d51a
Nate Sandholtz <nathan.sandholtz@gmail.com>
2020-03-02 16:01:21 -0800

    Added algorithm 1

--- code/simulation_utils.R
+++ code/simulation_utils.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(extraDistr)
 
 expit = function(x){exp(x)/(1+exp(x))}
 logit = function(p){log(p/(1-p))}
@@ -76,3 +77,64 @@ alter_theta = function(theta_draws,
     }
   return(logit(altered_theta_draws_probs))
 }
+
+########################################
+##########    ALGORITHM 1    ###########
+########################################
+
+algorithm_1 = function(s_0, 
+                       theta_draws,
+                       mu_draws,
+                       xi_draws,
+                       lambda_draws,
+                       c_0,
+                       L_dist,
+                       num_mcmc){
+  s_n = c_n = t_n = a_n = r_n = region = NA
+  
+  s_n[1] = s_0 
+  c_n[1] = c_0
+  
+  n = 1
+  while(s_n[n] != "turnover"){
+    region = strsplit(s_n[n], split = "_")[[1]][2]
+    draw_index = sample(1:num_mcmc,
+                        size = 1)
+    t_n[n] = get_t_int(c_n[n])
+    a_n[n] = ifelse(region == "heave",
+                    0,
+                    rbinom(1, 1, prob = expit(theta_draws[draw_index,s_n[n],t_n[n]])))
+    if(a_n[n] == 1){
+      reward_state = paste(strsplit(s_n[n], split = "_")[[1]][1],
+                           strsplit(s_n[n], split = "_")[[1]][2], sep = "_")
+      
+      r_n[n] = Reward(mu = mu_draws[draw_index, reward_state],
+                      xi = xi_draws[draw_index, region],
+                      def_open = strsplit(s_n[n], split = "_")[[1]][3] == "open",
+                      region = region)
+      break  
+    } else {
+      r_n[n] = 0
+    }
+    lapse = sample(L_dist[[t_n[n]]], 1)
+    c_n[n+1] = c_n[n] - lapse
+    if(c_n[n+1] < 0){
+      s_n[n+1] = "turnover"
+    } else {
+      s_n[n+1] = dimnames(lambda_draws)[[3]][rcat(1, prob = softmax(lambda_draws[draw_index, s_n[n], , t_n[n]]))]
+    }
+    n = n + 1
+  }
+  if (s_n[n] == "turnover") {
+    a_n[n] = NA
+    r_n[n] = 0
+    t_n[n] = get_t_int(c_n[n])
+  }
+  output = data.frame(state = s_n,
+                      action = a_n, 
+                      reward = r_n, 
+                      shot_clock = c_n,
+                      time_int = t_n,
+                      play = play)
+  return(output)
+} 
